8f91fdc043d0ff8da62abfd7649aec7048cbb330
matchalabs <matchalabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-19 10:27:00 +0000

    Fix responsive slides navigation positioning
    git-svn-id: https://plugins.svn.wordpress.org/ml-slider/trunk@700198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- preview.php
+++ preview.php
@@ -10,7 +10,7 @@ if (isset($_GET['slider_id']) && (int)$_GET['slider_id'] > 0) {
 
     $id = intval($_GET['slider_id']);
     echo "<html style='margin-top: 0 !important'><head>";
-    echo "</head><body style='overflow: hidden'>";
+    echo "</head><body style='overflow: hidden; margin: 0; padding: 0;'>";
     echo do_shortcode("[metaslider id={$id}]");
     wp_footer();
     echo "</body></html>";
