479d3faaab6d6383ea4723b594fc7fdec75b1c8f
matchalabs <matchalabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-27 18:16:19 +0000

    Add filter for extra admin options
    git-svn-id: https://plugins.svn.wordpress.org/ml-slider/trunk@704776 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ml-slider.php
+++ ml-slider.php
@@ -760,6 +760,9 @@ class MetaSliderPlugin {
                                     <input class='option coin' type='number' min='0' max='10000' step='100' name="settings[titleSpeed]" value='<?php echo $this->slider->get_setting('titleSpeed') ?>' /><?php _e("ms", 'metaslider') ?>
                                 </td>
                             </tr>
+
+                            <?php echo apply_filters('metaslider_admin_options', $this->slider->settings); ?>
+
                             <tr>
                                 <td colspan='2' class='highlight'><?php _e("Developer Options", 'metaslider') ?></td>
                             </tr>
