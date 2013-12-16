beaca8f764fbcfe255ee05911b965b582582c712
matchalabs <matchalabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-16 17:00:30 +0000

    Update version number & lang files
    git-svn-id: https://plugins.svn.wordpress.org/ml-slider/trunk@823288 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ml-slider.php
+++ ml-slider.php
@@ -3,7 +3,7 @@
  * Plugin Name: Meta Slider
  * Plugin URI: http://www.metaslider.com
  * Description: 4 sliders in 1! Choose from Nivo Slider, Flex Slider, Coin Slider or Responsive Slides.
- * Version: 2.5.1
+ * Version: 2.6-beta
  * Author: Matcha Labs
  * Author URI: http://www.matchalabs.com
  * License: GPLv2 or later
@@ -17,7 +17,7 @@
 // disable direct access
 if (!defined('ABSPATH')) exit;
 
-define('METASLIDER_VERSION', '2.5.1');
+define('METASLIDER_VERSION', '2.6-beta');
 define('METASLIDER_BASE_URL', plugins_url('ml-slider') . '/'); //plugin_dir_url(__FILE__)
 define('METASLIDER_ASSETS_URL', METASLIDER_BASE_URL . 'assets/');
 define('METASLIDER_BASE_DIR_LONG', dirname(__FILE__));
