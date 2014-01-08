2cc9020d9653b35f16fba41b610e1e075d08c083
matchalabs <matchalabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-08 13:39:47 +0000

    Add filters for slideshow HTML
    git-svn-id: https://plugins.svn.wordpress.org/ml-slider/trunk@834928 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/slider/metaslider.flex.class.php
+++ inc/slider/metaslider.flex.class.php
@@ -196,7 +196,7 @@ class MetaFlexSlider extends MetaSlider {
         $return_value .= "\n            </ul>";
         $return_value .= "\n        </div>";
 
-        return $return_value;
+        return apply_filters('metaslider_flex_slider_get_html', $return_value, $this->id, $this->settings);
     }
 }
 ?>
